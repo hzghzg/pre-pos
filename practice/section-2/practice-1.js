de78dc6cc3e2469748dc860ac4eb74d2386d09b3
hzghzg <42712812+hzghzg@users.noreply.github.com>
2019-07-09 00:38:07 +0800

    Add files via upload

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var arr=new Array();
+  var index=0;
+  for(var i1=0;i1<collection.length;i1++){
+    var obj={};
+    var count1=1;
+    if(collection[i1]!='0'){
+      for(var i2=i1+1;i2<collection.length;i2++){
+        if(collection[i1]==collection[i2]){
+          count1++;
+          collection[i2]='0';
+        }
+      }
+    obj.key=collection[i1];
+    obj.count=count1;
+    arr[index]=obj;
+    index++;
+    }
+  }
+  return arr;
 }
--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,45 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var arr=new Array();
+  var index=0;
+  for(var i1=0;i1<collection.length;i1++){
+    var obj={};
+    var count1=1;
+    var zimu;
+    var shuzi;
+    if(collection[i1].length>1){
+      zimu=collection[i1].split(/[-\[\]:]/)[0];
+      shuzi=parseInt(collection[i1].split(/[-\[\]:]/)[1]);
+    }
+    else
+    zimu=collection[i1];
+    if(zimu!='0'){
+      for(var i2=i1+1;i2<collection.length;i2++){
+        if(collection[i1]==collection[i2]){
+          count1++;
+          collection[i2]='0';
+        }
+        else if(collection[i2].length>1 && collection[i2].indexOf(zimu)>-1){
+          var shuzi1=parseInt(collection[i2].split(/[-\[\]:]/)[1]);
+          count1=count1+shuzi1;
+          collection[i2]='0';
+        }
+      }
+    if(count1!=1){
+      obj.name=zimu;
+      obj.summary=count1;
+      arr[index]=obj;
+    }
+    else{
+      if(collection[i1].length>1){
+        obj.name=zimu;
+        obj.summary=shuzi;
+        arr[index]=obj;
+      }
+    }
+    index++;
+    }
+  }
+  return arr;
 }
